587af9f9aa012a1d2093c3b8fd35623a6f73ec5c
gkc23456 <65560883+gkc23456@users.noreply.github.com>
2020-06-02 09:54:46 -0400

    Add files via upload

---
+++ Assignment 2 - Excercise 2.12.py
@@ -0,0 +1,7 @@
+# Exercise 2.12
+
+print('Amount on deposit after 10 years:', 1000 * (1 + .07) ** 10)
+
+print('Amount on deposit after 20 years:', 1000 * (1 + .07) ** 20)
+
+print('Amount on deposit after 30 years:', 1000 * (1 + .07) ** 30)
