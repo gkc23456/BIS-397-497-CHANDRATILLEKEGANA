b0328330591b46bd0c9af00c3a9d77b8677abc1d
gkc23456 <65560883+gkc23456@users.noreply.github.com>
2020-06-02 09:58:13 -0400

    Delete Assignment 2 - Excercise 3.6.py

--- Assignment 2 - Excercise 3.6.py
+++
@@ -1,9 +0,0 @@
-# Exercise 3.6
-input('What is your problem? ')
-
-response = input('Have you had this problem before (yes or no)? ')
-
-if response == 'yes':
-    print('Well, you have it again.')
-else:
-    print('Well, you have it now.')
