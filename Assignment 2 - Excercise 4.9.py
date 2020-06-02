f61274b0e64c0683227ea582027ec4be44c9ac37
gkc23456 <65560883+gkc23456@users.noreply.github.com>
2020-06-02 09:58:28 -0400

    Delete Assignment 2 - Excercise 4.9.py

--- Assignment 2 - Excercise 4.9.py
+++
@@ -1,8 +0,0 @@
-# Exercise 4.9
-def fahrenheit(celsius):
-    return (9 / 5) * celsius + 32
-
-print(f'Fahrenheit{"Celsius":>10}')
-
-for celsius in range(0,101):
-    print(f'{celsius:6}{fahrenheit(celsius):12.1f}')
